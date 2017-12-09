bcd06d5a6f2a48d37a8dd55e1ff8c3c473fb653f
Michael Lawrence BSc (HONS) <m2lawrence77@outlook.com>
2017-12-09 20:36:45 +0000

    Add files via upload

---
+++ RouteConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MVCEmployeeViewModel1
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //Here I set my new Controller to open up my new Index page at start up.
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "EmployeeViewModels", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
